64379c0d463774829b63ff3d812bbc3b85429525
nisse2112 <denisse.ugalde@ucr.ac.cr>
2023-12-08 14:41:38 -0600

    codificacion de canal

--- prueba_hamming/codificador_canal.py
+++ prueba_hamming/codificador_canal.py
@@ -1,23 +1,18 @@
-import re
-
 def binary_file_to_number_list(binary_file):
     """Lee un archivo binario y lo convierte en una lista de números."""
     with open(binary_file, "r") as file:
         binary_content = file.read()
 
     group_length = 7
-    bit_groups = re.findall('.{1,' + str(group_length) + '}', binary_content)
-    binary_list = [list(group) for group in bit_groups]
+    number_list = []
 
-    if len(bit_groups[-1]) < group_length:
-        binary_list[-1] += ['0'] * (group_length - len(bit_groups[-1]))
+    for i in range(0, len(binary_content), group_length):
+        group = binary_content[i:i+group_length].ljust(group_length, '0')
+        number_list.append([int(bit) for bit in group])
 
-    number_list = [[int(bit) for bit in group] for group in binary_list]
     return number_list
 
 def save_to_file(file_name, bit_list):
     """Guarda una lista de bits en un archivo."""
     with open(file_name, "w") as file:
-        for sublist in bit_list:
-            bits_str = "".join(str(bit) for bit in sublist)
-            file.write(bits_str)
+        file.writelines(''.join(map(str, sublist)) for sublist in bit_list)
--- prueba_hamming/hamming.py
+++ prueba_hamming/hamming.py
@@ -6,13 +6,13 @@ def create_binary_matrix(n, m):
 def create_identity_matrix(n, m):
     return np.pad(np.eye(min(n, m), dtype=int), ((0, n - min(n, m)), (0, m - min(n, m))), 'constant', constant_values=(0))
 
-def merge_matrices_horizontally(lti_matrix, id_matrix):
-    return np.concatenate((lti_matrix, id_matrix), axis=1)
+def merge_matrices_horizontally(m_ti, matriz_identidad):
+    return np.concatenate((m_ti, matriz_identidad), axis=1)
 
-def merge_matrices_vertically(matrix1, matrix2):
-    return np.concatenate((matrix2, matrix1), axis=0)
+def merge_matrices_vertically(m1, m2):
+    return np.concatenate((m2, m1), axis=0)
 
 def binary_dot_product(vector, matrix):
     if len(vector) != matrix.shape[0]:
-        raise ValueError("longitud del vector debe coincidir con la cantidad de filas")
+        raise ValueError("vector debe coincidir con numero de filas")
     return np.dot(vector, matrix) % 2
